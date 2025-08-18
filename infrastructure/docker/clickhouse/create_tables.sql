b05b3003911aaec908d5afcfab36d27ff985d1bb
Thu Phan <thuleanhphan@gmail.com>
2025-08-18 18:36:20 +0700

    [clickhouse] build: add init tables script for products & orders

--- infrastructure/docker/clickhouse/create_tables.sql
+++ infrastructure/docker/clickhouse/create_tables.sql
@@ -1,8 +1,19 @@
 CREATE DATABASE IF NOT EXISTS ecommerce_analytics;
-CREATE TABLE ecommerce_analytics.customers_cdc (
+CREATE TABLE IF NOT EXISTS ecommerce_analytics.customers_cdc (
     id Int64,
     name String,
     email String,
+    created_at Int64,
+    _version UInt64,
+    _deleted UInt8 DEFAULT 0
+) ENGINE = ReplacingMergeTree(_version)
+ORDER BY id;
+
+-- Products CDC table
+CREATE TABLE IF NOT EXISTS ecommerce_analytics.products_cdc (
+    id Int64,
+    name String,
+    price Decimal(10, 2),
     created_at Int64,
     _version UInt64,
     _deleted UInt8 DEFAULT 0
